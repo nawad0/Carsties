dc6f57427085a344a6079df103e7658af9ced9a5
Максим Дорошко <doroshko.m.v@gmail.com>
2024-03-27 23:00:50 +0300

    CRUDall

---
+++ src/AuctionService/Data/Migrations/20240327153413_RenameItem.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MilleAge",
+                table: "Items",
+                newName: "Mileage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Mileage",
+                table: "Items",
+                newName: "MilleAge");
+        }
+    }
+}
