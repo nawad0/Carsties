8f17659f9e6096fc0adffe763f2ee4f09df6d671
Максим Дорошко <doroshko.m.v@gmail.com>
2024-03-29 00:31:57 +0300

    SearchComplete

--- src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ src/SearchService/obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6f57427085a344a6079df103e7658af9ced9a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93af6af66f4af017cdf30ca0fb55502f156bc78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
