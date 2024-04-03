39ea842ebe46f9194c01261d9864d04eb23e3867
nawad0 <doarmax.max@gmail.com>
2024-04-03 23:39:59 +0300

    :)

---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, {useState} from 'react';
+import {Pagination} from "flowbite-react";
+type Props ={
+    currentPage: number
+    pageCount: number
+    pageChanged: (page: number) => void;
+}
+export default function AppPagination({currentPage,pageCount, pageChanged }:Props) {
+
+    return (
+        <Pagination
+            currentPage={currentPage}
+            onPageChange={e => pageChanged(e)}
+            totalPages={pageCount}
+            layout='pagination'
+            showIcons={true}
+            className='text-blue-500 mb-5'
+        />
+    )
+}
